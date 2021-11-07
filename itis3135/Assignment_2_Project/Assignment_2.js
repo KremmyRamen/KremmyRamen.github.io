70b251b59baa81d9dc4d32116eb752a89e5a3ab9
Raymond Nguyen <rnguye14@uncc.edu>
2021-11-07 16:36:13 -0500

    assignment 2
    assignment 2

---
+++ itis3135/Assignment_2_Project/Assignment_2.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	var imgs = new Array();
+	var intervalID;
+
+	$('#slideshow img').each(function() {
+		imgs.push($(this).attr('src'));
+	});
+
+	function setImage() 
+	{
+		var active = $('#active');
+		var currentImageURL = active.attr('src');
+		var activeImage = $.inArray(currentImageURL, imgs);
+		if(activeImage == (imgs.length - 1)){
+			activeImage = -1;
+		}
+
+		active.attr('src', imgs[activeImage + 1]);
+	}
+
+	$(function(){
+		intervalID = setInterval(setImage, 2500);
+	});
+});
