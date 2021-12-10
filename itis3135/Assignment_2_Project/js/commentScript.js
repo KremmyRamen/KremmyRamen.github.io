7273607c258251e3a7f9ef7f17c5050e3e1373fb
Raymond Nguyen <rnguye14@uncc.edu>
2021-12-10 17:09:42 -0500

    assignment 2
    assignment 2

--- itis3135/Assignment_2_Project/js/commentScript.js
+++ itis3135/Assignment_2_Project/js/commentScript.js
@@ -1,17 +1,44 @@
-
-
 $(document).ready(function(){
 
     var $comments = $('#comments');
+    var $name = $('#name');
+    var $comment = $('#comment');
+
+    function addComment(comment) {
+        $comments.append('<li>' + comment.name + ': ' + comment.comment + '</li><br />');
+    }
 
     $.ajax({
         type:'GET',
         url: 'apis/comments.json',
         success: function (comments) {
             $.each(comments, function (i, comment) {
-                $comments.append('<li>Name: ' + comment.name + '. Comment: ' + comment.comment + '</li>');
+                addComment(comment);
             });
+        },
+        error: function () {
+            alert('error loading comments');
         }
     });
 
+    $('#submit-comment').on('click', function () {
+        var newComment = {
+            name: $name.val(),
+            comment: $comment.val(),
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: 'apis/comments.json',
+            data: newComment,
+            success: function (newComment) {
+                addComment(newComment);
+            },
+            error: function () {
+                alert('error saving comment');
+            }
+        })
+    });
+
 });
+
